21db1ed887abec71624bb149fc83e5824ff81f96
DEV3L <jus.beall@gmail.com>
2024-08-07 17:32:25 -0400

    Make continue button optional in e2e test for google auth

--- assistify-ui/e2e/login.spec.ts
+++ assistify-ui/e2e/login.spec.ts
@@ -16,8 +16,14 @@ test('homepage has title and links to intro page', async ({ page }) => {
     await page.waitForSelector('input[type="password"]');
     await page.fill('input[type="password"]', process.env.GOOGLE_TEST_PASSWORD ?? ""); 
     await page.click('text=Next');
-    const continueButton = page.locator('text=Continue');
-    await continueButton.click();
+    
+    // Optional: Click the "Continue" button if it exists
+    try {
+      const continueButton = page.locator('text=Continue');
+      await continueButton.click();
+  } catch (e) {
+      console.log('Continue button not found, skipping...');
+  }
   
     // Wait for the redirect and assert the welcome message
     const welcomeMessage = `Welcome, ${process.env.GOOGLE_TEST_NAME ?? ""}`
