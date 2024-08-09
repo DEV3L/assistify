b74d88f3ac85f1c8a07c5381531e64ed8e814dfc
DEV3L <jus.beall@gmail.com>
2024-08-09 18:09:33 -0400

    Clean up end to end login test

--- assistify-ui/e2e/login.spec.ts
+++ assistify-ui/e2e/login.spec.ts
@@ -1,8 +1,9 @@
 import { expect, test } from "@playwright/test";
+import { loginToGoogle } from "./login.steps";
+
+const testUserName = process.env.GOOGLE_TEST_NAME ?? "";
 
 test("homepage has title and links to intro page", async ({ page }) => {
-  const accountName = process.env.GOOGLE_TEST_NAME ?? "";
-
   await page.goto("/login");
   await expect(page).toHaveTitle(/Assistify/);
 
@@ -10,70 +11,15 @@ test("homepage has title and links to intro page", async ({ page }) => {
   const signInButton = page.locator("text=Sign in with Google");
   await signInButton.click();
 
-  let isSessionLoggedInt = false;
-  try {
-    await page.waitForSelector("text=Sign Out", { timeout: 2500 });
-    isSessionLoggedInt = true;
-  } catch (error) {
-    console.error("Account selection button did not exist", error);
-  }
-
-  if (!isSessionLoggedInt) {
-    // Optional: Use existing session information
-    let isUsingExistingSession = false;
-    try {
-      await page.waitForSelector("text=Choose an account", { timeout: 2500 });
-      const accountSelectionButton = page.locator(`text=${accountName}`);
-      await accountSelectionButton.click();
-      isUsingExistingSession = true;
-    } catch (error) {
-      console.error("Account selection button did not exist", error);
-    }
-
-    if (!isUsingExistingSession) {
-      // Wait for the Google login page to load and select the first user
-      await page.waitForSelector('input[type="email"]');
-      await page.fill(
-        'input[type="email"]',
-        process.env.GOOGLE_TEST_EMAIL ?? ""
-      );
-      await page.click("text=Next");
-      await page.waitForSelector('input[type="password"]');
-      await page.fill(
-        'input[type="password"]',
-        process.env.GOOGLE_TEST_PASSWORD ?? ""
-      );
-      await page.click("text=Next");
-    }
-
-    // Optional: Click the "Continue" button if it exists
-    try {
-      const continueSelector = "text=Continue";
-      await page.waitForSelector(continueSelector, { timeout: 2500 });
-      const continueButton = page.locator(continueSelector);
-      await continueButton.click();
-    } catch (error) {
-      console.error("Continue button did not exist", error);
-    }
-
-    // Optional: Verify account
-    try {
-      const useAnotherPhoneSelector = "text=/use another phone/i";
-      await page.waitForSelector(useAnotherPhoneSelector, { timeout: 2500 });
-      const userAnotherPhone = page.locator(useAnotherPhoneSelector);
-      await userAnotherPhone.click();
-    } catch (error) {
-      console.error("Use Another Phone button did not exist", error);
-    }
-  }
+  await loginToGoogle(page);
 
   // Wait for the redirect and assert the welcome message
-  const welcomeMessage = `Welcome, ${process.env.GOOGLE_TEST_NAME ?? ""}`;
+  const welcomeMessage = `Welcome, ${testUserName}`;
   console.log(`"Welcome message: ${welcomeMessage}`);
 
   await page.waitForSelector(`text=${welcomeMessage}`);
   const welcomeElement = page.locator(`text=${welcomeMessage}`);
   await expect(welcomeElement).toBeVisible();
 
-  // await page.context().storageState({ path: 'storageState.json' });
+  // await saveStorageState(page);
 });
---
+++ assistify-ui/e2e/login.steps.ts
@@ -0,0 +1,35 @@
+import { Page } from "playwright";
+
+const isCI = !!process.env.CI;
+
+const testUserEmail = process.env.GOOGLE_TEST_EMAIL ?? "";
+const testUserPassword = process.env.GOOGLE_TEST_PASSWORD ?? "";
+
+/**
+ * Logs into Google using the provided Playwright page instance.
+ *
+ * @param {Page} page - The Playwright page instance to perform actions on.
+ * @returns {Promise<void>} - A promise that resolves when the login process is complete.
+ *
+ * This function handles the login process for Google. It checks if the environment is CI (Continuous Integration)
+ * and performs the login steps accordingly. If not in CI, it waits for the email input, fills it, clicks next,
+ * waits for the password input, fills it, clicks next, and finally clicks the continue button if it appears.
+ * The function waits for the "Sign Out" text to ensure the login was successful.
+ */
+export const loginToGoogle = async (page: Page): Promise<void> => {
+  if (!isCI) {
+    await page.waitForSelector('input[type="email"]');
+    await page.fill('input[type="email"]', testUserEmail);
+    await page.click("text=Next");
+    await page.waitForSelector('input[type="password"]');
+    await page.fill('input[type="password"]', testUserPassword);
+    await page.click("text=Next");
+
+    const continueSelector = "text=Continue";
+    await page.waitForSelector(continueSelector, { timeout: 2500 });
+    const continueButton = page.locator(continueSelector);
+    await continueButton.click();
+  }
+
+  await page.waitForSelector("text=Sign Out");
+};
