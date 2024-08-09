98cbf99be30c0230853e84bd111d2e15d098a9ef
DEV3L <jus.beall@gmail.com>
2024-08-09 09:55:25 -0400

    Add some playwright configuration to handle verifying identity

--- assistify-ui/e2e/login.spec.ts
+++ assistify-ui/e2e/login.spec.ts
@@ -1,37 +1,49 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
+test("homepage has title and links to intro page", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle(/Assistify/);
 
-test('homepage has title and links to intro page', async ({ page }) => {
-    await page.goto('/');
-    await expect(page).toHaveTitle(/Assistify/);
+  // Click the "Sign in with Google" button
+  const signInButton = page.locator("text=Sign in with Google");
+  await signInButton.click();
 
-    // Click the "Sign in with Google" button
-    const signInButton = page.locator('text=Sign in with Google');
-    await signInButton.click();
-  
-    // Wait for the Google login page to load and select the first user
-    await page.waitForSelector('input[type="email"]');
-    await page.fill('input[type="email"]', process.env.GOOGLE_TEST_EMAIL ?? ""); 
-    await page.click('text=Next');
-    await page.waitForSelector('input[type="password"]');
-    await page.fill('input[type="password"]', process.env.GOOGLE_TEST_PASSWORD ?? ""); 
-    await page.click('text=Next');
-    
-    // Optional: Click the "Continue" button if it exists
-    try {
-      const continueButton = page.locator('text=Continue');
-      if(await continueButton.isVisible()) {
-        await continueButton.click();
-      }
-  } catch (e) {
-      console.log('Continue button not found, skipping...');
+  // Wait for the Google login page to load and select the first user
+  await page.waitForSelector('input[type="email"]');
+  await page.fill('input[type="email"]', process.env.GOOGLE_TEST_EMAIL ?? "");
+  await page.click("text=Next");
+  await page.waitForSelector('input[type="password"]');
+  await page.fill(
+    'input[type="password"]',
+    process.env.GOOGLE_TEST_PASSWORD ?? ""
+  );
+  await page.click("text=Next");
+
+  // Optional: Click the "Continue" button if it exists
+  try {
+    const continueSelector = "text=Continue";
+    await page.waitForSelector(continueSelector, { timeout: 2500 });
+    const continueButton = page.locator(continueSelector);
+    await continueButton.click();
+  } catch (error) {
+    console.error("Continue button did not exist", error);
   }
 
-    // Wait for the redirect and assert the welcome message
-    const welcomeMessage = `Welcome, ${process.env.GOOGLE_TEST_NAME ?? ""}`
-    console.log(`"Welcome message: ${welcomeMessage}`);
+  // Optional: Click the "Continue" button if it exists
+  try {
+    const useAnotherPhoneSelector = "text=/use another phone/i";
+    await page.waitForSelector(useAnotherPhoneSelector, { timeout: 2500 });
+    const userAnotherPhone = page.locator(useAnotherPhoneSelector);
+    await userAnotherPhone.click();
+  } catch (error) {
+    console.error("Use Another Phone button did not exist", error);
+  }
 
-    await page.waitForSelector(`text=${welcomeMessage}`);
-    const welcomeElement = page.locator(`text=${welcomeMessage}`);
-    await expect(welcomeElement).toBeVisible();
-  });
+  // Wait for the redirect and assert the welcome message
+  const welcomeMessage = `Welcome, ${process.env.GOOGLE_TEST_NAME ?? ""}`;
+  console.log(`"Welcome message: ${welcomeMessage}`);
+
+  await page.waitForSelector(`text=${welcomeMessage}`);
+  const welcomeElement = page.locator(`text=${welcomeMessage}`);
+  await expect(welcomeElement).toBeVisible();
+});
--- assistify-ui/playwright.config.ts
+++ assistify-ui/playwright.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
 
 dotenv.config();
-dotenv.config({ path: '.env.local', override: true });
+dotenv.config({ path: ".env.local", override: true });
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: "./e2e",
   timeout: 60 * 1000,
   expect: {
     timeout: 5000,
@@ -14,21 +14,25 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: "html",
   use: {
     actionTimeout: 0,
-    baseURL: process.env.BASE_URL || 'http://localhost:3000',
-    trace: 'on-first-retry',
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
+    trace: "on-first-retry",
     headless: process.env.CI ? true : false,
-    screenshot: 'only-on-failure',
+    screenshot: "only-on-failure",
     launchOptions: {
-        args: ['--disable-blink-features=AutomationControlled'],
-      },
+      args: [
+        "--disable-blink-features=AutomationControlled",
+        "--no-sandbox-and-elevated",
+        "--disable-dev-shm-usage",
+      ],
+    },
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
