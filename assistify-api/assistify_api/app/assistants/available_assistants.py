275650ba7a84074004afac870ae54776fb2267bb
DEV3L <jus.beall@gmail.com>
2024-08-19 11:52:45 -0400

    Update list of whitelisted assistants

--- assistify-api/assistify_api/app/assistants/available_assistants.py
+++ assistify-api/assistify_api/app/assistants/available_assistants.py
@@ -1,10 +1,11 @@
 assistants_whitelist = [
-    "Jasper Bell - Knowledge Bot",
-    "Assistify - Product Owner",
-    "YouTube Creator",
     "AiDo - Virtual Head of Product - Alex Parker",
-    "Product Scout - Amazon Treasure Chat",
-    "Dr. Alistair Grey - Arxiv Librarian",
-    "R2-D2",
     "Alex Mercer - Artium Creator",
+    "Assistify - Product Owner",
+    "Dr. Alistair Grey - Arxiv Librarian",
+    "Justin Beall - Knowledge Bot",
+    "Product Scout - Amazon Treasure Chat",
+    "R2-D2",
+    "Recruiter Response",
+    "YouTube Creator",
 ]
