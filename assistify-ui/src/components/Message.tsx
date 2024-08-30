20800c836220c762cd1b62a9dd41425cf2fff53c
DEV3L <jus.beall@gmail.com>
2024-08-30 11:38:53 -0400

    Try fetching assistants in dashboard - troubleshoot 307 in prod

--- assistify-ui/src/components/Message.tsx
+++ assistify-ui/src/components/Message.tsx
@@ -1,16 +1,19 @@
 import LoadingSkeleton from "@/components/common/LoadingSkeleton";
+import { useFetchAssistants } from "@/services/assistants";
 import { usePostMessage } from "@/services/service";
+import { ListAssistantsResponse } from "@/types/AssistifyTypes";
 import { useEffect, useState } from "react";
 
 const ProtectedComponent = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<ListAssistantsResponse | null>(null);
   const { postMessage, isAuthenticated } = usePostMessage();
+  const { fetchAssistants } = useFetchAssistants();
 
   useEffect(() => {
     const fetchData = async () => {
       if (isAuthenticated) {
         try {
-          const result = await postMessage("Hello, world!");
+          const result = await fetchAssistants();
           setData(result);
         } catch (error) {
           console.error("Failed to fetch protected data:", error);
