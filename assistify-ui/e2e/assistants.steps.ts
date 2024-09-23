eb017434807711ee42e45e5a05bd37468db5423f
DEV3L <jus.beall@gmail.com>
2024-09-23 15:31:41 -0400

    Update e2e test to include user details

--- assistify-ui/e2e/assistants.steps.ts
+++ assistify-ui/e2e/assistants.steps.ts
@@ -3,9 +3,9 @@ import { Page } from "playwright";
 const expectedAssistantName = "Assistify - Concierge";
 
 export const validateAssistants = async (page: Page): Promise<void> => {
-  const assistantsIconSelector = '[data-testid="PersonIcon"]';
-  await page.waitForSelector(assistantsIconSelector);
-  await page.click(assistantsIconSelector);
+  const iconSelector = '[data-testid="SmartToyIcon"]';
+  await page.waitForSelector(iconSelector);
+  await page.click(iconSelector);
 
   await page.waitForSelector("text=Your Assistants");
   await page.waitForSelector(`text=${expectedAssistantName}`);
--- assistify-ui/e2e/smoke-test.spec.ts
+++ assistify-ui/e2e/smoke-test.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from "@playwright/test";
 import { validateAssistants } from "./assistants.steps";
 import { validateDashboard } from "./dashboard.steps";
 import { loginToGoogle } from "./login.steps";
+import { validateUserDetails } from "./user-details.steps";
 
 test("run smoke test", async ({ page }) => {
   await page.goto("/login");
@@ -10,6 +11,7 @@ test("run smoke test", async ({ page }) => {
   await loginToGoogle(page);
   await validateDashboard(page);
   await validateAssistants(page);
+  await validateUserDetails(page);
 
   // await saveStorageState(page);
 });
